7403ea8ec5e239e74956f8905f992d79a793c136
Ayush Sood <asood2_be19@thapar.edu>
2022-02-23 13:19:34 +0530

    downloaded ejs

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log('Error',err);
--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,6 @@ const router=express.Router();
 
 const usersController=require('../Controllers/users_controller');
 router.get('/profile',usersController.profile);
+const postcomments=require('../Controllers/post_comments');
+router.get('/postcomment',postcomments.profile);
 module.exports=router;
