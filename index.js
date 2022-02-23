46261cee99568ea65ee9b44c91e11343e91fbf27
Ayush Sood <asood2_be19@thapar.edu>
2022-02-23 13:05:01 +0530

    add multiple routers and controllers

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(typeof "arguments");
+const express=require('express');
+const app=express();
+const port=8000;
+
+app.listen(port,function(err){
+    if(err){
+        console.log('Error',err);
+    }
+    console.log('Server is running on port :${port}');
+})
+app.use('/',require('./routes'));
